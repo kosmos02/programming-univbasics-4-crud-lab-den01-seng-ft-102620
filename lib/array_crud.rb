6626af805308bbae147628567d67d56a44af1544
Alexander Gabriel <alexandrgabe@gmail.com>
2020-09-26 17:08:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -48,8 +48,12 @@ end
 
 def retrieve_last_element_from_array(array)
   
+  array[-1]
+  
 end
 
 def update_element_from_index(array, index_number, element)
+  
+  array[index_number] = element
 
 end
